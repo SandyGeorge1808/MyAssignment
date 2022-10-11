89bcd72b7d220486e6a6a1c7be07527454b4e1c7
SandyGeorge1808 <115519692+SandyGeorge1808@users.noreply.github.com>
2022-10-11 09:20:48 +0530

    Add files via upload
    Day1

---
+++ day3/Mobile.java
@@ -0,0 +1,21 @@
+package week1.day3;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String MobileBrand="iphone";
+		System.out.println(MobileBrand);
+		int ModelNumber=2567;
+		System.out.println(ModelNumber);
+		char  MobileLogo='I';
+		System.out.println(MobileLogo);
+		long MobileNumber=876543246l;
+		System.out.println(MobileNumber);
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+		double thePrice=45678.49078;
+		System.out.println(thePrice);
+		short noofMobilePiece=10;
+		System.out.println(noofMobilePiece);
+	}
+
+}
---
+++ day3/TwoWheelers.java
@@ -0,0 +1,28 @@
+package week1.day3;
+
+public class TwoWheelers {
+	public static void main(String[] args) {
+		int noofWheels=2;
+		System.out.println(noofWheels);
+		double TwoWheelersPrice=28345.29556;
+		System.out.println(TwoWheelersPrice);
+		char firstLetter='T';
+		System.out.println(firstLetter);
+		float frequency=34.6476543f;
+		System.out.println(frequency);
+		boolean isPuncture=true;
+		System.out.println(isPuncture);
+		String TwoWheelersName="Honda";
+		System.out.println(TwoWheelersName);
+		long registrationNumber=3678923467l;
+		System.out.println(registrationNumber);
+		
+		
+				
+				
+	
+		
+		
+	}
+
+}
